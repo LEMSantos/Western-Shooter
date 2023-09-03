d46cf06bb4f7e128aa4178cb3bccf26c0abd95b5
LEMSantos <lu.casmou.ra177833@gmail.com>
2023-09-03 08:21:25 -0300

    feat: adiciona o tamanho do tile e o título da janela

--- settings.py
+++ settings.py
@@ -1,6 +1,10 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+TILE_SIZE = 64
+
+GAME_TITLE = "Western Shooter"
+
 PATHS = {
     "player": "graphics/player",
     "coffin": "graphics/monster/coffin",
